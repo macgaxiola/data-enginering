c3ccbfbe7dda82676f054a84d2c6f35e5b7ae66a
Mac <mac.gaxiola@gmail.com>
2022-01-15 15:07:51 -0600

    another aproach

--- loadCSV.py
+++ loadCSV.py
@@ -57,9 +57,8 @@ def cvs_to_postgress_pandas():
     # connecting
     pg_hook = PostgresHook(postgress_conn_id='postgres_default').get_conn()
     curr = pg_hook.cursor()
-    
     for row in df.itertuples():
-        query = "INSERT INTO user_purchase (invoice_number, stock_code , detail , quantity , invoice_date , unit_price , customer_id, country) VALUES (%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s)" %(
+        query = "INSERT INTO user_purchase (invoice_number, stock_code , detail , quantity , invoice_date , unit_price , customer_id, country) VALUES ('{}','{}', {},'{}','{}','{}','{}','{}')".format(
             row.invoice_number,
             row.stock_code,
             row.detail,
